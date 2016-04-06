b98573779cb85d4bc51debbfa9e2f912330f3328
VaneOgando <nessaogando@gmail.com>
2016-04-05 22:30:05 -0430

    Detalles I
    Se arregla
    Detalle de carga de categoria en filtro

--- ProyectoSoporte/src/main/java/com/inventario/primefaces/beans/ConsultarInventarioBean.java
+++ ProyectoSoporte/src/main/java/com/inventario/primefaces/beans/ConsultarInventarioBean.java
@@ -55,12 +55,7 @@ public class ConsultarInventarioBean {
 		estados = consultarInventarioServicio.cargarEstados();
 		marcas = consultarInventarioServicio.cargarMarcas();
 
-		if (opcion.equals("0")) {        //Cargar equipos
-			System.out.println("ocultar bean");
-			RequestContext.getCurrentInstance().execute("ocultarCategoria();");
-		} else {                        //Cargar accesorios
-			System.out.println("mostrar bean");
-			RequestContext.getCurrentInstance().execute("mostrarCategoria();");
+		if (opcion.equals("1")) {
 			categorias = consultarInventarioServicio.cargarCategorias("accesorio");
 		}
 
